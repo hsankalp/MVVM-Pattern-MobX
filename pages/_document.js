3a725b6a8126f900a42c44082df8c0105bc32c90
Sankalp Heranjal <sankalpheranjal@SankalpHeranjal-MBP15TB-DET.local>
2021-02-01 20:22:12 -0500

    Bug fixes and Typescriptifying index

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,4 @@
-import Document, { Head, Main, NextScript } from "next/document";
+import Document, { Head, Main, NextScript, Html } from "next/document";
 import { ServerStyleSheet } from "styled-components";
 
 export default class MyDocument extends Document {
@@ -16,16 +16,13 @@ export default class MyDocument extends Document {
 
   render() {
     return (
-      <html>
-        <Head>
-          <title>My page</title>
-          {this.props.styleTags}
-        </Head>
+      <Html>
+        <Head>{this.props.styleTags}</Head>
         <body>
           <Main />
           <NextScript />
         </body>
-      </html>
+      </Html>
     );
   }
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,27 +4,27 @@ import Result from "../components/Result/index";
 import { Store, Context } from "../store/store";
 import styled from "styled-components";
 
-export default function Home() {
+const Container = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  margin: 32px;
+  gap: 32px;
+`;
+
+const Wrapper = styled.div`
+  width: 250px;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+  padding: 32px;
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+  @media (max-width: 600px) {
+    width: 100%;
+  }
+`;
+
+const Home = () => {
   const store = new Store();
-
-  const Container = styled.div`
-    display: flex;
-    flex-wrap: wrap;
-    margin: 32px;
-    gap: 32px;
-  `;
-
-  const Wrapper = styled.div`
-    width: 250px;
-    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
-    padding: 32px;
-    display: flex;
-    flex-direction: column;
-    gap: 16px;
-    @media (max-width: 600px) {
-      width: 100%;
-    }
-  `;
 
   return (
     <div>
@@ -47,4 +47,6 @@ export default function Home() {
       </main>
     </div>
   );
-}
+};
+
+export default Home;
