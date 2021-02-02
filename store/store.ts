50ed30013bcbb5debdc0690820df7a9918bdc76f
Sankalp Heranjal <sankalpheranjal@SankalpHeranjal-MBP15TB-DET.local>
2021-02-01 20:04:08 -0500

    babelrc added for sty;led components

---
+++ store/store.ts
@@ -0,0 +1,31 @@
+import { createContext } from "react";
+import { observable, computed, action, makeObservable } from "mobx";
+
+export class Store {
+  value1: number = 0;
+  value2: number = 0;
+
+  constructor() {
+    makeObservable(this, {
+      value1: observable,
+      value2: observable,
+      changeValue1: action,
+      changeValue2: action,
+      sum: computed,
+    });
+  }
+
+  changeValue1 = (): void => {
+    this.value1 = this.value1 + 5;
+  };
+
+  changeValue2 = (): void => {
+    this.value2 = this.value2 - 5;
+  };
+
+  get sum(): number {
+    return this.value1 + this.value2;
+  }
+}
+
+export const Context = createContext(new Store());
